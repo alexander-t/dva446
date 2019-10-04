7b0639e6c722c623dfa10f3ed0860dad305b31cd
Alexander Tarlinder <alexander.tarlinder@crisp.se>
2019-10-04 22:55:52 +0200

    Routes set up and static files added.

--- lab3/httpd.js
+++ lab3/httpd.js
@@ -1,7 +1,12 @@
 'use strict';
 const https = require('https');
 const fs = require('fs');
+const path = require('path');
 const express = require('express');
+
+const TEMPLATE_DIR = 'templates';
+
+const serverRoot = process.cwd();
 
 const sslOptions = {
     key: fs.readFileSync('cert/server.key'),
@@ -9,8 +14,34 @@ const sslOptions = {
 };
 
 const app = express();
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use('/', express.static('public', {index: false, redirect: false}));
+app.get('/', (req, res) => getRoot(req, res));
+app.post('/signin', (req, res) => postSignIn(req, res));
+app.post('/signup', (req, res) => postSignUp(req, res));
+app.post('/signout', (req, res) => postSignOut(req, res));
+app.post('/squeak', (req, res) => postSqueak(req, res));
 
 const server = https.createServer(sslOptions, app);
 
 server.listen(8000);
+
+// Routes
+function getRoot(req, res) {
+    res.sendFile(path.join(serverRoot, TEMPLATE_DIR, 'index.html'));
+}
+
+function postSignIn(req, res) {
+    console.log("/signin");
+}
+
+function postSignUp(req, res) {
+    console.log("/signup");
+}
+
+function postSignOut(req, res) {
+    console.log("/signin");
+}
+
+function postSqueak(req, res) {
+    console.log("/squeak");
+}
